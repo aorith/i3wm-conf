4d8714e344f04e86425e897cbc26a314e077a39b
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-05-27 19:50:59 +0200

    Clean

--- unlink.sh
+++ unlink.sh
@@ -7,42 +7,37 @@ _is_dir()  { [[ -d "$1" ]]; }
 _exists()  { [[ -e "$1" ]]; }
 _link()    { ln -sv "$1" "$2"; }
 
-_create_links() {
+_unlink() {
     local basename basedir file src tgt
     basename="$1" # home, config
     pushd "$basename" >/dev/null || return 1
     [[ -f basedir ]] || _err "Missing 'basedir' file in '$basename'."
     basedir=$(head -1 basedir)
-    echo "+ Linking folder/files in '$basename'..."
+    echo "+ UnLinking folder/files in '$basename'..."
     find . -maxdepth 1 -mindepth 1 \( -type f -o -type d \) -not -name 'basedir' -print0 | \
         while IFS= read -r -d '' file
     do
         [[ "$file" != "basedir" ]] || continue
         tgt="${HOME}/${basedir}/${file}"
         if _exists "$tgt"; then
-            tgt=$(realpath --no-symlinks "${HOME}/${basedir}/${file}") || exit 1
+            tgt=$(realpath --no-symlinks "$HOME/$basedir/$file") || exit 1
         fi
         src=$(realpath "$(pwd -P)/$file") || exit 1
 
         if _is_link "$tgt"; then
             if [[ $(realpath "$src") == $(realpath "$tgt") ]]; then
-                echo "   [OK] $tgt"
-                continue
+                rm -v "$tgt"
             else
                 _err "Target is a different link than expected, delete it: '$tgt'."
             fi
+        else
+            if _is_dir "$tgt"; then
+                _err "Target is a folder, delete it: '$tgt'."
+            fi
+            if _is_file "$tgt"; then
+                _err "Target is a file, delete it: '$tgt'."
+            fi
         fi
-
-        if _is_dir "$tgt"; then
-            _err "Target is a folder, delete it: '$tgt'."
-        fi
-
-        if _is_file "$tgt"; then
-            _err "Target is a file, delete it: '$tgt'."
-        fi
-
-        mkdir -p "$(dirname -- "$tgt")"
-        ln -sv "$src" "$tgt" || _err "Could not link '$tgt'."
     done
     popd >/dev/null || return 1
 }
@@ -50,5 +45,5 @@ _create_links() {
 find . -maxdepth 1 -mindepth 1 -type d -not -name '.git' -print0 | \
     while IFS= read -r -d '' folder
 do
-    _create_links "$folder" || exit 1
+    _unlink "$folder" || exit 1
 done
