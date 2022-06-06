cf028f8e2ce7cdcc35058f0ca8aadae46870c627
Manuel Sanchez Pinar <aomanu@gmail.com>
2022-06-06 11:00:57 +0200

    Add tmux

---
+++ _extras/tmux/compile-install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -ex
+cd "$(dirname "$0")" || exit 1
+
+REF="3.3"
+
+rm -rf tmux
+mkdir -p "${HOME}/.local/etc/xdg"
+
+echo "Installing dependencies..."
+sudo apt install automake libevent-dev
+
+git clone --depth=1 --branch="$REF" https://github.com/tmux/tmux.git
+rm -rf tmux/.git
+cd tmux
+
+./autogen.sh
+./configure
+make
+sudo make install
+
+cd ..
+rm -rf ./tmux
